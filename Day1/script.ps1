4f150febb9fa1047f4e2d36c489407ce42b5e107
Lukasz Jakubiak <75270330+eljot912@users.noreply.github.com>
2020-12-02 11:33:58 +0100

    Create script.ps1

---
+++ Day1/script.ps1
@@ -0,0 +1,39 @@
+$inputData = Get-Content .\input.txt
+$dataCount = $inputData.Count
+$result=[System.Collections.ArrayList]@()
+for ($i = 0; $i -lt $dataCount; $i++) {
+    $firstValue=$inputData[$i]
+    for ($j = 0; $j -lt $dataCount; $j++)
+    {
+        $secondValue=$inputData[$j]
+        if ($firstValue -ne $secondValue)
+        {
+            [int]$additionResult=[int]$firstValue + [int]$secondValue
+            if ($additionResult -eq 2020)
+            {
+                $result.Add([PSCustomObject]@{
+                    DataSet = "[$firstValue,$secondValue]"
+                    Addition = $additionResult
+                    Multiply = [int]$firstValue * [int]$secondValue
+                }) | Out-Null
+            }
+            for ($k = 0; $k -lt $dataCount; $k++)
+            {
+                $thirdValue=$inputData[$k]
+                if ($firstValue -ne $thirdValue)
+                {
+                    [int]$additionResult=[int]$firstValue + [int]$secondValue + [int]$thirdValue
+                    if ($additionResult -eq 2020)
+                    {
+                        $result.Add([PSCustomObject]@{
+                            DataSet = "[$firstValue,$secondValue,$thirdValue]"
+                            Addition = $additionResult
+                            Multiply = [int]$firstValue * [int]$secondValue * [int]$thirdValue
+                        }) | Out-Null
+                    }
+                }
+            }
+        }
+    }
+}
+$result | Sort-Object Multiply
