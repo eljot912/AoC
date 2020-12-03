99850c5bcb950a47d410a81b8c0e060063524d23
Lukasz Jakubiak <75270330+eljot912@users.noreply.github.com>
2020-12-03 10:51:02 +0100

    Create script.ps1

---
+++ Day3/script.ps1
@@ -0,0 +1,29 @@
+$puzzle = Get-Content ./input
+$direction = @('1.1','3.1','5.1','7.1','1.2')
+$direction | ForEach-Object {
+    $dRight=$_.split(".")[0]
+    $dDown=$_.split(".")[1]
+    $dIndex = 0
+    $slope = 0
+    $dTrees = 0
+    $puzzle | ForEach-Object {
+        if ($slope % $dDown -eq 0) {
+            $end=$_.length-1
+            if ($dIndex -gt $end) {
+                $extend = [math]::Truncate($dIndex / $end) + 1
+                $line = $_* $extend
+            }
+            else {
+                $line = $_
+            }
+            if($line[$dIndex] -eq '#') {
+                $dTrees++
+            }
+            $dIndex+=$dRight
+        }
+        $slope++
+    }
+    Write-Output "Trees at $_`: $dTrees"
+    if ([int]$trees -eq 0 ) { $trees=$dTrees } else { $trees *= $dTrees }
+}
+Write-Output "Multiply all: $trees"
