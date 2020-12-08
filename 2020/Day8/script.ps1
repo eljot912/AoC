3dcc8df9f66c2ad78e2bc27dd5b88f6b28dd2665
Lukasz Jakubiak <75270330+eljot912@users.noreply.github.com>
2020-12-08 16:45:52 +0100

    Update script.ps1

--- 2020/Day8/script.ps1
+++ 2020/Day8/script.ps1
@@ -2,78 +2,39 @@ $ErrorActionPreference='Stop'
 $in = Get-Content $PSScriptRoot/input
 $t=[System.Collections.ArrayList]@()
 $r=[System.Collections.ArrayList]@()
-$tr=[System.Collections.ArrayList]@()
+$v=[System.Collections.ArrayList]@()
 [int]$ins=1
 
 $in | ForEach-Object {
-$t.Add([PSCustomObject]@{
-    ID = $ins
-    CMD = $_.split(" ")[0]
-    ARG = $_.split(" ")[1]
-    }) | Out-Null
-    $ins++
+    $t.Add([PSCustomObject]@{
+        ID = $ins
+        CMD = $_.split(" ")[0]
+        ARG = $_.split(" ")[1]
+        }) | Out-Null
+        $ins++
 }
 
-#stage #1
-[int]$acc=0
-[int]$nextCMD = 1
-[int]$pLines=0
-foreach ($line in $t ) {
-    $command = $t  | Where-Object {$_.ID -eq $nextCMD }
-    switch ($command.CMD) {
-        "nop" {
-            #does nothing
-            $nextCMD++
-        }
-        "acc" {
-            $acc += $command.ARG
-            $nextCMD++
-        }
-        "jmp" {
-            $nextCMD += $command.ARG
-        }
-    }
-    $pLines++
-    $r.Add($command.ID) | Out-Null
-    if ($nextCMD -in $r) {
-        break
-    }
-}
-$r.Clear()
-if($pLines -ne $t.Count) {
-    Write-Output "Stage #1: Loop Attempt. ACC: $acc"
-}
-
-#stage #2
-$v = $t | Where-Object {$_.CMD -in ('nop','jmp')}
-foreach($v1 in $v)
+$IdList=($t | Where-Object {$_.CMD -in ('nop','jmp')}) + [PSCustomObject]@{ID = 0}
+$v.Add(($IdLIst | Sort-Object ID)) | Out-null
+foreach($v1 in $v.ID)
 {
-    $t | ForEach-Object {
-        if ($v1.ID -eq $_.ID) {
-            switch ($_.CMD) {
-                "nop" { $rep = "jmp" }
-                "jmp" { $rep = "nop" }
-            }
-        }
-        else {
-            $rep=$_.CMD            
-        }
-        $tr.Add([PSCustomObject]@{
-            ID = $_.ID
-            CMD = $rep
-            ARG = $_.ARG
-            }) | Out-Null
-    }
-    
-    #checking different instructions (swapped jmp <-> nop iteration)
     [int]$acc=0
     [int]$nextCMD = 1
     [int]$pLines=0
-    foreach ($line in $tr ) {
-        $command = $tr  | Where-Object {$_.ID -eq $nextCMD }
-        switch ($command.CMD) {
+    foreach ($line in $t ) {
+        $command = $t  | Where-Object {$_.ID -eq $nextCMD }
+        if ($command.ID -eq $v1) {
+            switch ($command.CMD) {
+                "nop" { $cmd = "jmp" }
+                "jmp" { $cmd = "nop" }
+            }
+        }
+        else {
+            $cmd=$command.CMD         
+
+        }
+        switch ($cmd) {
             "nop" {
-                #does nothing
                 $nextCMD++
             }
             "acc" {
@@ -91,10 +52,11 @@ foreach($v1 in $v)
         }
     }
     $r.Clear()
-    if($pLines -eq $t.Count) {
+    if($pLines -ne $t.Count -and $v1 -eq 0) {
+        Write-Output "Stage #1. Loop. ACC: $acc"
+    }
+    elseif($pLines -eq $t.Count) {
         Write-Output "Stage #2. Completed. ACC: $acc"
-        break
     }
     $r.Clear()
-    $tr.Clear()
 }
