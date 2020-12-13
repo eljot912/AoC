bb49ecee56de4c23931924d1561012831d58594f
eljot <eljot912@gmail.com>
2020-12-13 23:20:23 +0100

    Day13

---
+++ 2020/Day13/script.ps1
@@ -0,0 +1,38 @@
+$ErrorActionPreference = 'Stop'
+$in = Get-Content $PSScriptRoot/input
+$h=[ordered]@{}
+$depart=$in[0]
+$t=[System.Collections.ArrayList]@()
+
+[int]$count=0
+$in[1].split(',') | ForEach-Object {
+    if($_ -ne 'x') {
+        $h["$count"] = [PSCustomObject]@{HID=$count;ID=$_}
+    }
+    $count++
+}
+
+$h.Values | ForEach-Object {
+    $waitTime=$_.ID - ($depart % $_.ID)
+    $t.Add([PSCustomObject]@{
+        ID = $_.ID
+        Time = $waitTime
+    }) | Out-Null
+}
+$r=($t | Sort-Object Time | Select-Object -First 1)
+$st1=[int]$r.ID * [int]$r.Time
+
+$buses=($h.GetEnumerator() | Sort-Object Name ).value
+[long]$time=0
+[long]$offset=$buses[0].ID
+foreach ($bus in $buses) {
+    while (([long]$time + [long]$bus.hID) % $bus.ID -ne 0) {
+        $time += $offset
+    }
+    if ($bus.HID -ne 0) {
+        [long]$offset *= [long]$bus.ID
+    }
+}
+$st2=$time
+
+Write-Output "Stage #1: -> $st1 | Stage #2: -> $st2"
