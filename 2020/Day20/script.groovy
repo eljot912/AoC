16342153d5bf7dda3356ffe7a596640f0b3341ff
Lukasz Jakubiak <75270330+eljot912@users.noreply.github.com>
2021-01-08 17:08:24 +0100

    Update script.groovy

--- 2020/Day20/script.groovy
+++ 2020/Day20/script.groovy
@@ -1,4 +1,4 @@
-def get_input() {
+def getInput() {
 	File file = new File('example')
     input=file.text.replace('\r','').split('\n\n')
     outputMap=[:]
@@ -10,23 +10,18 @@ def get_input() {
     return outputMap
 }
 
-def get_border(tileArray,edge) {
+def getBorder(tileArray,edge) {
 	String border = ''
 	switch(edge) {
-		case 'n':
-			border = tileArray[0]; break
-		case 's':
-			border = tileArray[-1]; break
-		case 'e':
-			border = (tileArray.collect { it[-1] }).join(); break
-		case 'w':
-			border = (tileArray.collect { it[0] }).join(); break
+		case 'n': border = tileArray[0]; break
+		case 's': border = tileArray[-1]; break
+		case 'e': border = (tileArray.collect { it[-1] }).join(); break
+		case 'w': border = (tileArray.collect { it[0] }).join(); break
 	}
 	return border
 }
 
-
-def match_puzzles(tilesMap) {
+def matchPuzzles(tilesMap) {
     matchSide = [:]
     cornersMap = [:]
     tileCombos = [tilesMap.keySet(),tilesMap.keySet()].combinations().findAll{a,b-> a <b}
@@ -37,8 +32,8 @@ def match_puzzles(tilesMap) {
 		sides = ['n','s','e','w']
 		for (edge_1 in sides) {
 			for (edge_2 in sides) {
-				border_1 = get_border(tile_1, edge_1)
-				border_2 = get_border(tile_2, edge_2)
+				border_1 = getBorder(tile_1, edge_1)
+				border_2 = getBorder(tile_2, edge_2)
 				if (border_1 == border_2 || border_1 == border_2.reverse() ) {
 					matchSide[id_1] = matchSide.containsKey(id_1) ? matchSide[id_1] +  edge_1:  edge_1
 					matchSide[id_2] = matchSide.containsKey(id_2) ? matchSide[id_2] +  edge_2:  edge_2
@@ -54,24 +49,47 @@ def match_puzzles(tilesMap) {
 	return cornersMap
 }
 
-puzzles =  get_input()
-corners =  match_puzzles(puzzles)
-Long stage_1 = 1
-corners.keySet().collect {stage_1*=it}
-println "Stage_1: -> ${stage_1}"
-
 def rotateObject(tileObject) {
     rotated  = []
     for (column in 0..tileObject[0].size()-1) {
         rotated_row = ''
         for (row in tileObject[-1..-(tileObject.size())]) {
-            rotated_row += row[column]
+            rotated_row+=row[column]
         }
-        rotated+=rotated_row
+        rotated.add(rotated_row)
     }
     return rotated
 }
 
+def tileArranges(inputTile) {
+	arranges = []
+	arranges.add(inputTile)
+	flipTile = inputTile.reverse()
+	for (r in 1..3) {
+		inputTile = rotateObject(inputTile)
+		arranges.add(inputTile)
+	}
+	arranges.add(flipTile)
+	for (r in 1..3) {
+		flipTile = rotateObject(flipTile)
+		arranges.add(flipTile)
+	}
+	return arranges
+}
+
+def removeBorder(inputTile) {
+	strippedTile=(inputTile[1..inputTile.size()-2]).collect { it[1..it.size()-2] }
+	//strippedTile=strippedTile.collect { it[1..it.size()-2] }
+	return strippedTile
+}
+
+puzzles =  getInput()
+corners =  matchPuzzles(puzzles)
+Long stage_1 = 1
+corners.keySet().collect {stage_1*=it}
+println "Stage_1: -> ${stage_1}"
+
+imageSize=Math.sqrt(puzzles.size()) as Integer
 cornerPuzzleID = corners.keySet()[0]
 cornerPuzzleEdges = corners[cornerPuzzleID]
 cornerPuzzle = puzzles[cornerPuzzleID]
@@ -86,5 +104,3 @@ else if (cornerPuzzleEdges in ['nw', 'wn']) {
 else if (cornerPuzzleEdges in ['sw','ws']) {
 	cornerPuzzle = rotateObject(rotateObject(rotateObject(cornerPuzzle)))
 }
-
-imageSize=Math.sqrt(puzzles.size())
